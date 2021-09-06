653984cc4a9fee676deea9334a61442d7aa609be
alexanderschaer <schaeralexander@gmail.com>
2021-09-06 18:16:33 +0200

    add yaml-content for testing, add integration tests for login

--- backend/src/test/java/de/neuefische/CapStone/backend/controller/AuthControllerTest.java
+++ backend/src/test/java/de/neuefische/CapStone/backend/controller/AuthControllerTest.java
@@ -1,11 +1,15 @@
 package de.neuefische.CapStone.backend.controller;
 
+import de.neuefische.CapStone.backend.api.AccessToken;
+import de.neuefische.CapStone.backend.api.Credentials;
 import de.neuefische.CapStone.backend.api.User;
+import de.neuefische.CapStone.backend.config.JwtConfig;
 import de.neuefische.CapStone.backend.model.UserEntity;
 import de.neuefische.CapStone.backend.repo.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.matchers.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -14,8 +18,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import javax.swing.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
@@ -32,13 +35,20 @@ class AuthControllerTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    AuthControllerTest(UserRepository userRepository, PasswordEncoder passwordEncoder, TestRestTemplate testRestTemplate, JwtConfig jwtConfig) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.testRestTemplate = testRestTemplate;
+        this.jwtConfig = jwtConfig;
+    }
+
     private String url() { return "http://localhost:" + port;}
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private TestRestTemplate testRestTemplate;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TestRestTemplate testRestTemplate;
+    private final JwtConfig jwtConfig;
 
     @AfterEach
     public void clearUserRepo() {userRepository.deleteAll();}
@@ -99,19 +109,84 @@ class AuthControllerTest {
         //THEN
         assertThat(response.getStatusCode(), is(HttpStatus.CONFLICT));
     }
+
     @Test
-    public void userNameI() {
+    public void userLogsIn() {
         //GIVEN
-        User userToAdd = User.builder()
-                .userName("")
-                .email("Bowser@supermario.com")
-                .password("ImInLoveWithMario").build();
-        String newUrl = url() + "/user/registration";
+        String encodedPassword= passwordEncoder.encode("password");
+        String userName = "Bowser";
+        String email = "Bowser@supermario.com";
+        String role = "user";
+        String password = "password";
+        userRepository.save(
+                UserEntity.builder()
+                        .userName(userName)
+                        .email(email)
+                        .role(role)
+                        .password(encodedPassword).build()
+        );
+
+        Credentials credentials = Credentials.builder()
+                .userName(userName)
+                .password(password).build();
+        String newUrl = url() + "/user/login";
+
         //WHEN
-        HttpEntity<User> httpEntity = new HttpEntity<>(userToAdd);
-        ResponseEntity<User> response = testRestTemplate.exchange(newUrl, HttpMethod.POST, httpEntity, User.class);
+      //  HttpEntity<Credentials> httpEntity = new HttpEntity<>(credentials);
+        ResponseEntity<AccessToken> response = testRestTemplate.postForEntity(newUrl, credentials, AccessToken.class);
+
         //THEN
-        assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        String token = response.getBody().getToken();
+        Claims claims = Jwts.parser().setSigningKey(jwtConfig.getSecret()).parseClaimsJws(token).getBody();
+        assertThat(claims.getSubject(), is(userName));
+        assertThat(claims.get("email", String.class), is(email));
+
+    }
+    @Test
+    public void unregisteredUserTriesToLogin() {
+        //GIVEN
+
+        Credentials credentials = Credentials.builder()
+                .userName("userName")
+                .password("password").build();
+        String newUrl = url() + "/user/login";
+
+        //WHEN
+      //  HttpEntity<Credentials> httpEntity = new HttpEntity<>(credentials);
+        ResponseEntity<AccessToken> response = testRestTemplate.postForEntity(newUrl, credentials, AccessToken.class);
+
+        //THEN
+        assertThat(response.getStatusCode(), is(HttpStatus.UNAUTHORIZED));
+    }
+
+    @Test
+    public void userTriesToLoginWithWrongPassword() {
+        //GIVEN
+        String encodedPassword= passwordEncoder.encode("password");
+        String userName = "Bowser";
+        String email = "Bowser@supermario.com";
+        String role = "user";
+        String password = "wrongpassword";
+        userRepository.save(
+                UserEntity.builder()
+                        .userName(userName)
+                        .email(email)
+                        .role(role)
+                        .password(encodedPassword).build()
+        );
+
+        Credentials credentials = Credentials.builder()
+                .userName(userName)
+                .password(password).build();
+        String newUrl = url() + "/user/login";
+
+        //WHEN
+        //  HttpEntity<Credentials> httpEntity = new HttpEntity<>(credentials);
+        ResponseEntity<AccessToken> response = testRestTemplate.postForEntity(newUrl, credentials, AccessToken.class);
+
+        //THEN
+        assertThat(response.getStatusCode(), is(HttpStatus.UNAUTHORIZED));
     }
 
 }

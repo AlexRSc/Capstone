421971dc40159ed0e863e3ffaef2be8dfcc42e29
alexanderschaer <schaeralexander@gmail.com>
2021-09-08 14:58:51 +0200

    add Basic Layout for lights page, still in need for refactor into components

--- frontend/src/pages/LightsPage.js
+++ frontend/src/pages/LightsPage.js
@@ -1,26 +1,84 @@
-import {useAuth} from "../auth/AuthProvider";
 import PageLayout from "../components/PageLayout";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+import {FormControlLabel, Slider, Switch} from "@material-ui/core";
 
 export default function LightsPage() {
     return (
         <PageLayout>
             <Header title="Connect My Hub"/>
             <Wrapper>
-                <h1>This is a PlaceHolder</h1>
-                <p>Whatever, whenever</p>
-                <p>We´re meant to be together</p>
-                <p>I´ll be there, and you´ll be near</p>
-                <p>And that´s the deal, my dear</p>
-                <Link to ="/home"><Button color="primary" variant="contained">Back</Button></Link>
+                <Button variant="contained" color="primary"
+                        size="large" startIcon={<SaveIcon/>}>
+                    <Link class="Links" to="/lights">Add Lights Device</Link>
+                </Button>
+                <LightsWrapper>
+                    <Light>
+                        <FormControlLabel control={<Switch color="primary"/>} label={"Device 1"}
+                                          labelPlacement="top"/>
+                        <Slider size="small" orientation="vertical"/>
+                    </Light>
+                    <Light>
+                        <FormControlLabel control={<Switch color="primary"/>} label={"Device 2"}
+                                          labelPlacement="top"/>
+                        <Slider size="small" orientation="vertical"/>
+                    </Light>
+                    <Light>
+                        <FormControlLabel control={<Switch color="primary"/>} label={"Device 3"}
+                                          labelPlacement="top"/>
+                        <Slider size="small" orientation="vertical"/>
+                    </Light>
+                    <Light>
+                        <FormControlLabel control={<Switch color="primary"/>} label={"Device 4"}
+                                          labelPlacement="top"/>
+                        <Slider size="small" orientation="vertical"/>
+                    </Light>
+                    <Light>
+                        <FormControlLabel control={<Switch color="primary"/>} label={"Device 3"}
+                                          labelPlacement="top"/>
+                        <Slider size="small" orientation="vertical"/>
+                    </Light>
+                    <Light>
+                        <FormControlLabel control={<Switch color="primary"/>} label={"Device 4"}
+                                          labelPlacement="top"/>
+                        <Slider size="small" orientation="vertical"/>
+                    </Light>
+                </LightsWrapper>
+                <Link to="/home"><Button color="primary" variant="contained">Back</Button></Link>
             </Wrapper>
             <Footer/>
         </PageLayout>
     )
 }
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  display: grid;
+  place-items: center;
+  flex-direction: column;
+  grid-template-rows: 1fr 2fr 1fr;
+
+  .Links {
+    text-decoration: none;
+    color: white;
+  }
+`
+
+const LightsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+
+const Light = styled.div`
+  justify-content: space-between;
+  margin-right: 10px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  
+`

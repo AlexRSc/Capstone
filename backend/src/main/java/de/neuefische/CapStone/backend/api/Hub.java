3573715fe7b653bc93d9937579323a5009d7dfc4
alexanderschaer <schaeralexander@gmail.com>
2021-09-08 10:33:21 +0200

    add createHub functionality with Controller+service+repo+entity

--- backend/src/main/java/de/neuefische/CapStone/backend/api/Hub.java
+++ backend/src/main/java/de/neuefische/CapStone/backend/api/Hub.java
@@ -1,2 +1,16 @@
-package de.neuefische.CapStone.backend.api;public class Hub {
+package de.neuefische.CapStone.backend.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Hub {
+
+    private String hubEmail;
+    private String hubPassword;
 }
--- backend/src/main/java/de/neuefische/CapStone/backend/repo/HubRepository.java
+++ backend/src/main/java/de/neuefische/CapStone/backend/repo/HubRepository.java
@@ -1,2 +1,12 @@
-package de.neuefische.CapStone.backend.repo;public interface HubRepository {
+package de.neuefische.CapStone.backend.repo;
+
+import de.neuefische.CapStone.backend.model.HubEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HubRepository extends JpaRepository<HubEntity, Long> {
+
+    Boolean existsByHubEmail(String hubEmail);
+
 }

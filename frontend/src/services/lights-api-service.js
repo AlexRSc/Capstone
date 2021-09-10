0a3103098ce19f2b382e0779fbddedddff8c76df
alexanderschaer <schaeralexander@gmail.com>
2021-09-10 19:58:06 +0200

    add full frontend lights onOff&brightness functionality

--- frontend/src/services/lights-api-service.js
+++ frontend/src/services/lights-api-service.js
@@ -9,3 +9,19 @@ const headers = token => ({
 export const addLights = (token, credentials) =>
     axios.post('api/ProfBeluga/lights/add', credentials, headers(token))
         .then(response => response.data)
+
+export const getMyLights = (token) =>
+    axios.get('api/ProfBeluga/lights/mylightdevices', headers(token))
+        .then(response => response.data)
+
+export const turnLightOn = (token, light) =>
+    axios.post('api/ProfBeluga/lights/turnon',light, headers(token))
+        .then(response => response.data)
+
+export const turnLightOff = (token, light) =>
+    axios.post('api/ProfBeluga/lights/turnoff',light, headers(token))
+        .then(response => response.data)
+
+export const setBrightness = (token, brightness) =>
+    axios.post('api/ProfBeluga/lights/brightness',brightness , headers(token))
+        .then(response => response.data)

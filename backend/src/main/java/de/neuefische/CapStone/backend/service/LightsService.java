1c122c3c300a5c92933a6c1421fcf1abec9c16d8
alexanderschaer <schaeralexander@gmail.com>
2021-09-10 19:52:19 +0200

    fixed a little within lights in Backend

--- backend/src/main/java/de/neuefische/CapStone/backend/service/LightsService.java
+++ backend/src/main/java/de/neuefische/CapStone/backend/service/LightsService.java
@@ -1,7 +1,9 @@
 package de.neuefische.CapStone.backend.service;
 
 import de.neuefische.CapStone.backend.api.LightDevice;
+import de.neuefische.CapStone.backend.model.HubEntity;
 import de.neuefische.CapStone.backend.model.LightsDeviceEntity;
+import de.neuefische.CapStone.backend.repo.HubRepository;
 import de.neuefische.CapStone.backend.repo.LightsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,15 +15,21 @@ import java.util.Optional;
 public class LightsService {
 
     private final LightsRepository lightsRepository;
+    private final HubRepository hubRepository;
 
 
     @Autowired
-    public LightsService(LightsRepository lightsRepository) {
+    public LightsService(LightsRepository lightsRepository, HubRepository hubRepository) {
         this.lightsRepository = lightsRepository;
+        this.hubRepository = hubRepository;
     }
 
     public LightsDeviceEntity create(LightsDeviceEntity lightsDeviceEntity) {
-
+        Optional<HubEntity> hubEntityOptional= hubRepository.findHubEntityByUserName(lightsDeviceEntity.getDevice().getUserName());
+        if (hubEntityOptional.isEmpty()){
+            throw new IllegalArgumentException("You don´t have a Hub");
+        }
+        lightsDeviceEntity.getDevice().setHubId(hubEntityOptional.get().getId());
         return lightsRepository.save(lightsDeviceEntity);
     }
 
